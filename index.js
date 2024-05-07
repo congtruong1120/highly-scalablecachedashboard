213be0c74d48c414ef414119dc31862a4425f49a
congtruong1120 <kieranmarsnam1311@outlook.com>
2024-05-07 16:14:53 +0700

    First commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const factorialRecursive = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  return n * factorialRecursive(n - 1);
-};
+function pathSum(root, sum) {
+  if (!root) return false;
+  if (!root.left && !root.right && root.val === sum) return true;
+  return (
+    pathSum(root.left, sum - root.val) || pathSum(root.right, sum - root.val)
+  );
+}
